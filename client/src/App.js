00334c8ff89242fbbf83a7491b94ff47adb22309
Adam Hoffman <wintros@yahoo.com>
2020-03-18 09:34:28 -0600

    Cards component skeleton

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,13 @@
 import React from 'react';
 
+import Cards from './components/Cards.js'
+
 
 
 function App() {
   return (
     <div>
-      
+      <Cards />
     </div>
   );
 }
---
+++ client/src/components/Cards.js
@@ -0,0 +1,11 @@
+import React, { useState } from 'react'
+
+import './Cards.css'
+
+const Cards = () => {
+    return (
+        <div></div>
+    )
+}
+
+export const Cards
